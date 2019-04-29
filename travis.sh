0861c97b3b526fd15163cc74fec66e016377fa86
Sebastian Dengler <dengler@toposens.de>
2019-04-29 14:21:46 +0200

    updated travis.sh

--- travis.sh
+++ travis.sh
@@ -5,6 +5,11 @@ echo "in travis.sh"
 echo "$TRAVIS_BUILD_DIR"
 echo "$basename ($TRAVIS_BUILD_DIR)"
 echo "$dirname ($TRAVIS_BUILD_DIR)"
+GITHUB_URL="https://github.com"
+URL=${TRAVIS_BUILD_DIR/"/home/travis/build"/$GITHUB_URL}
+echo URL
+URL=${TRAVIS_BUILD_DIR//home/travis/build/$GITHUB_URL}
+echo URL
 
 : '
 # Run Docker container
