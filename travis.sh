441bd86296864d50f342c1e6b474862c162fad74
Sebastian Dengler <dengler@toposens.de>
2019-04-29 09:41:36 +0200

    updated travis.sh

--- travis.sh
+++ travis.sh
@@ -5,7 +5,11 @@ echo "in travis.sh"
 if ! [ "$IN_DOCKER" ]; then
   echo "if in docker"
   docker pull $DOCKER_IMAGE
-  docker run -t -d $DOCKER_IMAGE
+
+  docker run -t -d \
+  -e IN_DOCKER=true \
+  $DOCKER_IMAGE
+
   docker exec $(docker ps -q) bash -c "cd /home/ && git clone https://github.com/sebdengler/travis-test.git"
   docker exec $(docker ps -q) bash -c "cd /home/travis-test/ && source travis.sh"
   exit
