d25d76a330e2661373121532196a52dafc1b2491
Sebastian Dengler <dengler@toposens.de>
2019-04-29 09:46:34 +0200

    updated travis.sh

--- travis.sh
+++ travis.sh
@@ -17,9 +17,5 @@ fi
 
 echo "in docker container"
 
-#docker pull toposens/toposens
-
-#docker run -t -d toposens/toposens
-#docker exec $(docker ps -q) bash -c "cd /home/ && git clone https://github.com/sebdengler/travis-test.git"
-#docker exec $(docker ps -q) bash -c "cd /home/travis-test/ && source ros_setup.bash"
-#docker ps
+# ROS setup
+source ros_setup.bash
